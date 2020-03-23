18bdab92f72ada900e615aab9a65113ddd8e04a8
Alexandros Theodotou <alex@zrythm.org>
2020-03-23 21:09:19 +0000

    use correct lib extensions in the tests

--- scripts/lv2_validate_wrap.sh
+++ scripts/lv2_validate_wrap.sh
@@ -11,14 +11,15 @@ LV2_VALIDATE_BIN=$1
 PL_BUILD_DIR=$2
 PL_NAME=$3
 PL_URI=$4
+LIBEXT=$5
 
-tmpdir=$(mktemp -d /tmp/lv2lint_wrap.XXXXXXXXX)
+tmpdir=$(mktemp -d /tmp/lv2_validate_wrap.XXXXXXXXX)
 tmp_plugin_dir="$tmpdir/$PL_NAME"
 mkdir -p "$tmp_plugin_dir"
 cp $PL_BUILD_DIR/$PL_NAME.ttl \
-  $PL_BUILD_DIR/${PL_NAME}_dsp.so $tmp_plugin_dir/
-if [ -e $PL_BUILD_DIR/${PL_NAME}_ui.so ] ; then
-  cp $PL_BUILD_DIR/${PL_NAME}_ui.so $tmp_plugin_dir/
+  $PL_BUILD_DIR/${PL_NAME}_dsp$LIBEXT $tmp_plugin_dir/
+if [ -e $PL_BUILD_DIR/${PL_NAME}_ui$LIBEXT ] ; then
+  cp $PL_BUILD_DIR/${PL_NAME}_ui$LIBEXT $tmp_plugin_dir/
 fi
 cp $PL_BUILD_DIR/$3_manifest.ttl $tmp_plugin_dir/manifest.ttl
 
