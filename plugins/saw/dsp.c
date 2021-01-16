646587a0f6a3a61b0378d1168568a2642f19df0f
Alexandros Theodotou <alex@zrythm.org>
2021-01-16 23:41:02 +0000

    ZSaw: fix double free on deactivate

--- plugins/saw/dsp.c
+++ plugins/saw/dsp.c
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2019-2020 Alexandros Theodotou <alex at zrythm dot org>
  *
- * This file is part of ZSaw
+ * This file is part of ZPlugins
  *
- * ZSaw is free software: you can redistribute it and/or modify
+ * ZPlugins is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
  *
- * ZSaw is distributed in the hope that it will be useful,
+ * ZPlugins is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU General Affero Public License
- * along with ZSaw.  If not, see <https://www.gnu.org/licenses/>.
+ * along with ZPlugins.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include PLUGIN_CONFIG
@@ -681,8 +681,8 @@ deactivate (
         {
           sp_blsaw_destroy (&key->blsaws[j]);
         }
-      sp_destroy (&self->sp);
     }
+  sp_destroy (&self->sp);
 }
 
 static void
